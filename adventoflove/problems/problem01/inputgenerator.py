79a1e4c662447c8023a2f6376cc47f3c53cfa600
Setzer777 <lguitarras0594@gmail.com>
2016-01-10 11:59:19 -0600

    First Commit

---
+++ adventoflove/problems/problem01/inputgenerator.py
@@ -0,0 +1,10 @@
+from random import randint
+
+generatedInput = ""
+for i in range(10000):
+    if randint(0, 1) == 0:
+        generatedInput += '<'
+    else:
+        generatedInput += '3'
+
+print generatedInput
