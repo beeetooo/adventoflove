a6910fc4d580d291828ebd775e3211ea800f25e9
Setzer777 <lguitarras0594@gmail.com>
2016-01-10 22:55:11 -0600

    gets rid of a JS deprecated script to load a problem, because instead we load it via Flask

--- static/js/loadProblem.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-    $.ajax({
-        url : "/static/input/01",
-        dataType: "text",
-        success : function (data) {
-            $("#problem").html(data);
-        }
-    });
-});
